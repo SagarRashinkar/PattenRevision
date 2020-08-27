8dcac0dc57467f79a3ff7a86470a9111d2a36d9e
Sagar <sagarrashinkar2@gmail.com>
2020-08-27 23:27:08 +0530

    ADDED: First name

--- Pattern.sh
+++ Pattern.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Welcome"
+
+#UC 1 =>
+
+read -p "enter first name:" fname
+pattern=^[A-Z]{1}[a-z]{2}
+if [[ $fname =~ $pattern ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
